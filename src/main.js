b7636259fdb2a2e8f3b382e432e744da8933cb47
LGWen <gaowen.liu@yz-intelligence.com>
2020-05-25 16:50:57 +0800

    mockjs

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import './style/global.css'
 import store from './store'
 import router from './router'
+
 import './style/'
 import './core/lazy_use'
+
+// 菜单权限处理
 import './permission'
+// 本地数据mock 正式打包请删除
 import './mock'
 Vue.config.productionTip = false
 
