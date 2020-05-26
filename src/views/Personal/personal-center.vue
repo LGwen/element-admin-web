61f29b4e79ec5395ff94bf290180631d8041b994
GWen <gaowen.liu@yz-intelligence.com>
2020-05-26 14:27:27 +0800

    修改个人中心

--- src/views/Personal/personal-center.vue
+++ src/views/Personal/personal-center.vue
@@ -75,7 +75,7 @@
     <el-col :xs="24" :sm="18" :md="18" :lg="18" :xl="18">
       <el-card>
         <el-tabs v-model="activeName" @tab-click="handleClick">
-          <el-tab-pane label="消息 (5)" name="first">
+          <el-tab-pane :label="'消息 ('+4+')'" name="first">
             <ul v-loading="loading" class="message-list" style="min-height:100px;">
               <li v-for="m in messages" :key="m.id" class="message-item">
                 <div class="message-item-header">
@@ -96,14 +96,17 @@
               </li>
             </ul>
           </el-tab-pane>
-          <el-tab-pane label="缺陷" name="second">
+          <el-tab-pane :label="'缺陷 ('+4+')'" name="second">
             <ul v-loading="loading" class="message-list" style="min-height:100px;">
               <li v-for="m in problems" :key="m.id" class="message-item">
                 <div class="message-item-header">
                   <div class="message-item-header__title">
                     <h4>{{ m.title }}</h4>
                   </div>
-                  <div class="message-item-header__sub">from：张大山</div>
+                  <div class="message-item-header__sub">
+                    <span>创建人：张大山</span>
+                    <span>处理人：张大山</span>
+                  </div>
                 </div>
                 <div class="message-item-content">{{ m.content }}</div>
                 <div class="message-item-footer">
@@ -111,13 +114,56 @@
                   <div>
                     <el-button type="text">查看详情</el-button>
                     <el-divider direction="vertical"></el-divider>
-                    <el-button type="text">删除</el-button>
+                    <el-button type="text">处理</el-button>
                   </div>
                 </div>
               </li>
             </ul>
           </el-tab-pane>
-          <el-tab-pane label="新需求" name="third">新需求</el-tab-pane>
+          <el-tab-pane label="我的项目" name="third">
+            <e-list>
+              <e-list-item v-for="l in list" :key="l.id">
+                <el-avatar
+                  slot="avatar"
+                  shape="square"
+                  :size="50"
+                  fit="cover"
+                  src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+                ></el-avatar>
+                <span slot="name">{{ l.name }}</span>
+                <div slot="desc">{{ l.desc }}</div>
+                <template slot="content">
+                  <div class="list-item__content-item">
+                    <span>负责人</span>
+                    <p>{{ l.person }}</p>
+                  </div>
+                  <div class="list-item__content-item">
+                    <span>预计结束时间</span>
+                    <p>{{ l.finishDate }}</p>
+                  </div>
+                  <div>
+                    <div style="width:200px;display: inline-block;margin-left: 20px;">
+                      <el-progress :percentage="parseInt(Math.random()*50+50)" style="width:200px"></el-progress>
+                    </div>
+                  </div>
+                </template>
+                <template slot="action">
+                  <el-button type="text">编辑</el-button>
+                  <el-divider direction="vertical"></el-divider>
+                  <el-dropdown>
+                    <el-button type="text">
+                      更多
+                      <i class="el-icon-arrow-down el-icon--right"></i>
+                    </el-button>
+                    <el-dropdown-menu slot="dropdown">
+                      <el-dropdown-item>删除</el-dropdown-item>
+                      <el-dropdown-item>延期</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </template>
+              </e-list-item>
+            </e-list>
+          </el-tab-pane>
         </el-tabs>
       </el-card>
     </el-col>
@@ -126,12 +172,56 @@
 
 <script>
 import { mapState, mapActions } from "vuex";
+import List from "@/components/List/index.js";
+const EList = List.EList;
+const EListItem = List.EListItem;
 export default {
   name: "PersonalCenter",
+  components: {
+    EList,
+    EListItem
+  },
   data() {
     return {
       activeName: "first",
-      loading: false
+      loading: false,
+      list: [
+        {
+          name: "潇湘出行",
+          desc: "智能公交，精准预计公交到达时间",
+          person: "张大千",
+          finishDate: "2020-05-20",
+          id: 1
+        },
+        {
+          name: "我的长沙",
+          desc: "政务服务平台，便民利民",
+          person: "张大千",
+          finishDate: "2020-05-20",
+          id: 2
+        },
+        {
+          name: "数字星城联盟",
+          desc: "企业信息发布，放利企业",
+          person: "张大千",
+          finishDate: "2020-05-20",
+          id: 3
+        },
+        {
+          name: "一车一码",
+          desc: "防疫小程序",
+          person: "张大千",
+          finishDate: "2020-05-20",
+          id: 4
+        },
+        {
+          name: "生活圈",
+          desc: "一手掌握周边重要生活基础设施",
+          person: "张大千",
+          finishDate: "2020-05-20",
+          id: 5
+        }
+      ],
     };
   },
   computed: {
@@ -240,6 +330,9 @@ export default {
         float: right;
         font-size: 13px;
         color: rgba(0, 0, 0, 0.45);
+        span {
+          margin-left: 20px;
+        }
       }
     }
     &-content {
